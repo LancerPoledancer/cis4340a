9287a254d94e8844e338aecf028a7d1e6b495476
Grayson <47420624+LancerPoledancer@users.noreply.github.com>
2025-03-02 21:13:06 -0500

    Create R05_OBJ05_J.java

---
+++ R05_OBJ05_J.java
@@ -0,0 +1,11 @@
+class ListUtility {
+  private static void addToList(List<String> list, String str) {
+    list.add(str);     // No warning generated
+  }
+ 
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<String> ();
+    addToList(list, "42");
+    System.out.println(list.get(0));
+  }
+}
